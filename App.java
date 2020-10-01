cec999ddc7111e8bcb9d6be93f7bdf0099b4a52f
marceloAzevedo <marcelo@triider.com>
2020-10-01 20:50:04 -0300

    first version

--- App.java
+++ App.java
@@ -1,31 +1,92 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class App {
+    private static byte[] memory = new byte[1000];
+    private static ArrayList<Character> source = new ArrayList<>();
+    private static ArrayList<Integer> ife = new ArrayList<>();
+    private static int data_pointer = 0;// memoria
+    private static int program_counter = 0;// source
+    private static final char[] op = {'<', '>', '[', ']', '$', '+', '-', '.', ','};
     public static void main(String[] args) {
-      // Scanner read = new Scanner(System.in);
-
       try {
-        String pathFile = "/home/marcelo/Documents/interpretador/SOURCE";
-
-        FileReader file = new FileReader(pathFile);
-        BufferedReader readFile = new BufferedReader(file);
-   
-        String line = readFile.readLine(); 
-        System.out.println("Teste 2");
-        while (line != null) {
-
-          line = readFile.readLine(); 
-          System.out.println("line " + line );
-        }     
-        
-        file.close();
+        readFile(source, ife);
+        //while(caractere != '$')
+        for(var caractere : source) {
+          switch(caractere) {
+            case '<':
+              data_pointer--;
+              break;
+            case '>':
+              data_pointer++;
+              break;
+            case '[':
+              break;
+            case ']':
+              break;
+            case '$':
+              break;
+            case '+':
+              memory[data_pointer]++;
+              break;
+            case '-':
+              memory[data_pointer]--;
+              break;
+            case ',':
+              memory[data_pointer] = ife.remove(0).byteValue();
+              break;
+            case '.':
+              escreve_no_of(memory[data_pointer]);
+              break;
+            default:
+              break;
+          }
+        }
       } catch (IOException e) {
-          System.err.printf("Erro na abertura do arquivo: %s.\n",
-            e.getMessage());
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+        
+      }
+     
+    }
+
+    public static void readFile(ArrayList<Character> source, ArrayList<Integer> ife) throws IOException {
+      final String source_path = "/home/marcelo/Documents/interpretador/SOURCE";
+      final String if_path = "/home/marcelo/Documents/interpretador/IF";
+      try {
+        
+        //read source
+        FileReader file = new FileReader(source_path);
+        BufferedReader readFile = new BufferedReader(file);
+        int c = 0;
+
+        while((c = readFile.read()) != -1) {
+          source.add((char) c);
+        }
+
+        file.close();
+
+        //read ife
+        file =  new FileReader(if_path);
+        readFile = new BufferedReader(file);
+        String line = readFile.readLine();
+        while(line != null) {
+          ife.add(Integer.parseInt(line));
+        }
+
+        file.close();
+
+      } catch (IOException e) {
+        System.err.printf("Erro na abertura do arquivo: %s.\n",
+        e.getMessage());
       }    
+      
+    }
+
+    public static void escreve_no_of(byte a) {
+
     }
   }
